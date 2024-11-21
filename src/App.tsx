33c8d4715731599287093f5c65ab8568e75fb98d
junggwanhun <wjdrhksgns7602@gmail.com>
2024-11-22 00:13:25 +0900

    feature: rn-helper 리액트 마이그레이션 작업 11/22

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import Navbar from './components/Navbar';
 import { Outlet } from 'react-router-dom';
+import './index.css';
 
 const App = () => {
     return (
-        <>
-            <Navbar />
-            <main>
+        <div className="flex justify-center items-center h-screen w-full bg-gray-100">
+            {/* 화면 높이에 꽉 차도록 설정 */}
+            <div className="w-full max-w-[375px] h-full bg-white p-4 shadow-lg">
+                {/* Outlet은 자식 컴포넌트가 여기에 렌더링됨 */}
                 <Outlet />
-            </main>
-        </>
+            </div>
+        </div>
     );
 };
 

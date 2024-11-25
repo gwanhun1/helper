3395772c31637398ae6f55ed1c39f74b5e414e72
gwanhun1 <wjdrhksgns7602@gmail.com>
2024-11-25 23:53:10 +0900

    feature: rn-helper 리액트 마이그레이션 작업 11/25

---
+++ src/components/templates/WeekendLogs.tsx
@@ -0,0 +1,34 @@
+import { AiOutlineMail } from 'react-icons/ai';
+import IconRow from '../molecules/IconRow';
+import TitleContainer from '../molecules/TitleContainer';
+
+const WeekendLogs = ({ setSelect }: any) => {
+    return (
+        <>
+            <TitleContainer title="주간" />
+            <div className="my-2 cursor-pointer" onClick={() => setSelect(1)}>
+                <IconRow
+                    icon={<AiOutlineMail className="w-6 h-6 text-gray-500" />}
+                    mainText="제목"
+                    subText="내용"
+                />
+            </div>
+            <div className="my-2 cursor-pointer" onClick={() => setSelect(1)}>
+                <IconRow
+                    icon={<AiOutlineMail className="w-6 h-6 text-gray-500" />}
+                    mainText="제목"
+                    subText="내용"
+                />
+            </div>
+            <div className="my-2 cursor-pointer" onClick={() => setSelect(1)}>
+                <IconRow
+                    icon={<AiOutlineMail className="w-6 h-6 text-gray-500" />}
+                    mainText="제목"
+                    subText="내용"
+                />
+            </div>
+        </>
+    );
+};
+
+export default WeekendLogs;

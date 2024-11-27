5385186678654fa607f89f61eac6606b1029b8b0
정관훈 <wjdrhksgns7602@gmail.com>
2024-11-27 15:58:46 +0900

    feature: rn-helper 리액트 마이그레이션 작업 11/27

---
+++ src/components/organisms/UserNotice.tsx
@@ -0,0 +1,11 @@
+import IconRow from "../molecules/IconRow";
+
+const UserNotice = () => {
+  return (
+    <>
+      <h2 className="my-4 text-lg font-bold text-gray-900">Notice</h2>
+      <IconRow mainText="공지1" subText="공지1" />
+    </>
+  );
+};
+export default UserNotice;
--- src/components/templates/WeekendLogs.tsx
+++ src/components/templates/WeekendLogs.tsx
@@ -1,33 +1,30 @@
-import { AiOutlineMail } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
 import IconRow from "../molecules/IconRow";
 import TitleContainer from "../molecules/TitleContainer";
+import Accordion from "../atoms/Accordion";
 
-type WeekendLogsProps = {
-  setSelect: React.Dispatch<React.SetStateAction<number>>;
-};
-
-const WeekendLogs = ({ setSelect }: WeekendLogsProps) => {
+const WeekendLogs = () => {
   return (
     <>
       <TitleContainer title="주간" />
       <div className="my-2 cursor-pointer" onClick={() => setSelect(1)}>
         <IconRow
-          icon={<AiOutlineMail className="w-6 h-6 text-gray-500" />}
-          mainText="제목"
+          icon={<AiOutlineUser className="w-6 h-6 text-gray-500" />}
+          mainText={<Accordion title="제목" content="내용" />}
           subText="내용"
         />
       </div>
       <div className="my-2 cursor-pointer" onClick={() => setSelect(1)}>
         <IconRow
-          icon={<AiOutlineMail className="w-6 h-6 text-gray-500" />}
-          mainText="제목"
+          icon={<AiOutlineUser className="w-6 h-6 text-gray-500" />}
+          mainText={<Accordion title="제목" content="내용" />}
           subText="내용"
         />
       </div>
       <div className="my-2 cursor-pointer" onClick={() => setSelect(1)}>
         <IconRow
-          icon={<AiOutlineMail className="w-6 h-6 text-gray-500" />}
-          mainText="제목"
+          icon={<AiOutlineUser className="w-6 h-6 text-gray-500" />}
+          mainText={<Accordion title="제목" content="내용" />}
           subText="내용"
         />
       </div>

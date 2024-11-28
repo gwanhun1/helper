0e71c056908d1b76d8f52c5b04addb1f86b5bcc6
gwanhun1 <wjdrhksgns7602@gmail.com>
2024-11-28 23:11:39 +0900

    feature: rn-helper 리액트 마이그레이션 작업 11/28

--- src/components/molecules/Accordion.tsx
+++ src/components/molecules/Accordion.tsx
@@ -1,61 +1,70 @@
-import { useState } from "react";
-import { FaPlusCircle, FaMinusCircle } from "react-icons/fa";
-import { AiOutlineRobot } from "react-icons/ai";
+import { useState } from 'react';
+import { FaPlusCircle, FaMinusCircle } from 'react-icons/fa';
+import { AiOutlineRobot } from 'react-icons/ai';
+import useSelectTreeStore from '../../store/selectTreeStore';
 
 type AccordionProps = {
-  title: string;
-  content: string;
-  width?: number;
+    title: string;
+    content: string;
+    width?: number;
 };
 
 const Accordion = ({ title, content, width }: AccordionProps) => {
-  const [isCollapsed, setIsCollapsed] = useState(true);
+    const [isCollapsed, setIsCollapsed] = useState(true);
+    const { content: selectContent, response: selectResponse } =
+        useSelectTreeStore();
 
-  const toggleAccordion = () => {
-    setIsCollapsed(!isCollapsed);
-  };
+    const toggleAccordion = () => {
+        setIsCollapsed(!isCollapsed);
+    };
+    console.log(selectContent);
+    console.log(title === selectContent);
 
-  return (
-    <div
-      className={`my-4 rounded-lg shadow-md ${
-        width ? `w-[${width}%]` : "w-full"
-      }`}
-    >
-      <button
-        onClick={toggleAccordion}
-        className="flex items-center justify-between w-full p-3 bg-white rounded-lg border border-b-2"
-      >
-        <p
-          className={`text-sm max-w-52 ${
-            isCollapsed
-              ? "truncate whitespace-nowrap overflow-hidden"
-              : "break-words text-left"
-          }`}
+    return (
+        <div
+            className={`mt-2 rounded-lg shadow-md  ${
+                width ? `w-[${width}%]` : 'w-full'
+            }`}
         >
-          {title}
-        </p>
-        <div className="flex items-center justify-center w-6 h-6 ml-2">
-          {isCollapsed ? (
-            <FaPlusCircle className="text-gray-500" size={16} />
-          ) : (
-            <FaMinusCircle className="text-gray-500" size={16} />
-          )}
+            <button
+                onClick={toggleAccordion}
+                className={`${
+                    selectContent == title && selectResponse === content
+                        ? 'bg-green-200'
+                        : 'bg-white'
+                } flex items-center justify-between w-full p-3  border border-b-2 rounded-lg`}
+            >
+                <p
+                    className={`text-sm max-w-52 ${
+                        isCollapsed
+                            ? 'truncate whitespace-nowrap overflow-hidden'
+                            : 'break-words text-left'
+                    }`}
+                >
+                    {title}
+                </p>
+                <div className="flex items-center justify-center w-6 h-6 ml-2">
+                    {isCollapsed ? (
+                        <FaPlusCircle className="text-gray-500" size={16} />
+                    ) : (
+                        <FaMinusCircle className="text-gray-500" size={16} />
+                    )}
+                </div>
+            </button>
+            <div
+                className={`transition-all duration-300 ease-in-out overflow-hidden ${
+                    isCollapsed ? 'h-0 opacity-0' : 'h-auto opacity-100'
+                }`}
+            >
+                <div className="flex items-center justify-center px-4 pt-2 pb-4">
+                    <div className="flex-shrink-0 p-2 mr-2 rounded-lg bg-slate-100">
+                        <AiOutlineRobot className="w-4 h-4 text-gray-500" />
+                    </div>
+                    <p className="text-xs text-gray-500 ">{content}</p>
+                </div>
+            </div>
         </div>
-      </button>
-      <div
-        className={`transition-all duration-300 ease-in-out overflow-hidden ${
-          isCollapsed ? "h-0 opacity-0" : "h-auto opacity-100"
-        }`}
-      >
-        <div className="px-4 pb-4 pt-2 flex justify-center items-center">
-          <div className="flex-shrink-0 p-2 rounded-lg bg-slate-100 mr-2">
-            <AiOutlineRobot className="w-4 h-4 text-gray-500" />
-          </div>
-          <p className="text-xs text-gray-700 ">{content}</p>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Accordion;
--- src/components/organisms/LogForm.tsx
+++ src/components/organisms/LogForm.tsx
@@ -1,20 +1,22 @@
-import { AiOutlineUser } from "react-icons/ai";
-import IconRow from "../molecules/IconRow";
-import Accordion from "../molecules/Accordion";
-import { LogItem } from "../../hooks/useLogData";
+import { AiOutlineUser } from 'react-icons/ai';
+import IconRow from '../molecules/IconRow';
+import Accordion from '../molecules/Accordion';
+import { LogItem } from '../../hooks/useLogData';
 
 type LogFormProps = { data: LogItem };
 
 const LogForm = ({ data }: LogFormProps) => {
-  return (
-    <div className="my-1 cursor-pointer">
-      <IconRow
-        icon={<AiOutlineUser className="w-6 h-6 text-gray-500" />}
-        mainText={<Accordion title={data.content} content={data.response} />}
-        subText={data.date}
-      />
-    </div>
-  );
+    return (
+        <div className="my-1 cursor-pointer">
+            <IconRow
+                icon={<AiOutlineUser className="w-6 h-6 text-gray-500" />}
+                mainText={
+                    <Accordion title={data.content} content={data.response} />
+                }
+                subText={data.date}
+            />
+        </div>
+    );
 };
 
 export default LogForm;

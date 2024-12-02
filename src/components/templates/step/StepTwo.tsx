3d71d4e8f83f3d4d2f12f2d8d03db31fc8aa995a
정관훈 <wjdrhksgns7602@gmail.com>
2024-12-02 13:49:07 +0900

    feature: rn-helper 리액트 마이그레이션 작업 12/2

--- src/components/templates/step/StepTwo.tsx
+++ src/components/templates/step/StepTwo.tsx
@@ -7,6 +7,11 @@ const StepTwo = () => {
   const { increase, decrease } = useStepStore();
   const { setWho } = useWorryStore();
 
+  const handleWhoSelection = (who: string) => {
+    setWho(who);
+    increase();
+  };
+
   return (
     <div className="p-2">
       <TitleContainer
@@ -25,44 +30,142 @@ const StepTwo = () => {
           outline
         />
         <Button
-          onPress={() => setWho("1")}
-          text="Short"
+          onPress={() => handleWhoSelection("엄마")}
+          text="엄마"
           bgColor="bg-green-700"
         />
         <Button
-          onPress={() => increase()}
-          text="A Little Longer Text"
+          onPress={() => handleWhoSelection("아빠")}
+          text="아빠"
           bgColor="bg-green-700"
         />
         <Button
-          onPress={() => increase()}
-          text="Medium Text"
+          onPress={() => handleWhoSelection("형/오빠")}
+          text="형/오빠"
           bgColor="bg-green-700"
         />
         <Button
-          onPress={() => increase()}
-          text="Another Button with Text"
+          onPress={() => handleWhoSelection("동생")}
+          text="동생"
           bgColor="bg-green-700"
         />
         <Button
-          onPress={() => increase()}
-          text="Dynamic Button"
+          onPress={() => handleWhoSelection("친구")}
+          text="친구"
           bgColor="bg-green-700"
         />
         <Button
-          onPress={() => increase()}
-          text="Ask Me Anything"
+          onPress={() => handleWhoSelection("선생님")}
+          text="선생님"
           bgColor="bg-green-700"
         />
         <Button
-          onPress={() => increase()}
-          text="This is a Much Longer Button Text"
+          onPress={() => handleWhoSelection("헬스트레이너")}
+          text="헬스트레이너"
           bgColor="bg-green-700"
         />
         <Button
-          onPress={() => increase()}
-          text="Final Button"
+          onPress={() => handleWhoSelection("할머니")}
+          text="할머니"
           bgColor="bg-green-700"
+        />
+        <Button
+          onPress={() => handleWhoSelection("할아버지")}
+          text="할아버지"
+          bgColor="bg-green-700"
+        />
+        <Button
+          onPress={() => handleWhoSelection("동네 아저씨")}
+          text="동네 아저씨"
+          bgColor="bg-green-700"
+        />
+        <Button
+          onPress={() => handleWhoSelection("백수")}
+          text="백수"
+          bgColor="bg-green-700"
+        />
+        <Button
+          onPress={() => handleWhoSelection("재벌 회장님")}
+          text="재벌 회장님"
+          bgColor="bg-green-700"
+        />
+        <Button
+          onPress={() => handleWhoSelection("연예인")}
+          text="연예인"
+          bgColor="bg-green-700"
+        />
+        <Button
+          onPress={() => handleWhoSelection("CEO")}
+          text="CEO"
+          bgColor="bg-green-700"
+        />
+
+        <Button
+          onPress={() => handleWhoSelection("학생")}
+          text="유치원생"
+          bgColor="bg-green-700"
+        />
+
+        <Button
+          onPress={() => handleWhoSelection("이웃")}
+          text="백수 이웃"
+          bgColor="bg-green-700"
+        />
+        <Button
+          onPress={() => handleWhoSelection("미래 자녀")}
+          text="자녀"
+          bgColor="bg-green-700"
+        />
+
+        <Button
+          onPress={() => handleWhoSelection("외계인")}
+          text="외계인"
+          bgColor="bg-blue-700"
+        />
+        <Button
+          onPress={() => handleWhoSelection("타임머신 타고 온 사람")}
+          text="타임머신 타고 온 사람"
+          bgColor="bg-yellow-700"
+        />
+        <Button
+          onPress={() => handleWhoSelection("만화 캐릭터")}
+          text="만화 캐릭터"
+          bgColor="bg-purple-700"
+        />
+        <Button
+          onPress={() => handleWhoSelection("로봇")}
+          text="로봇"
+          bgColor="bg-gray-700"
+        />
+        <Button
+          onPress={() => handleWhoSelection("스파이")}
+          text="괴물"
+          bgColor="bg-red-700"
+        />
+        <Button
+          onPress={() => handleWhoSelection("슈퍼히어로")}
+          text="슈퍼히어로"
+          bgColor="bg-orange-700"
+        />
+        <Button
+          onPress={() => handleWhoSelection("악당")}
+          text="악당"
+          bgColor="bg-black"
+        />
+        <Button
+          onPress={() => handleWhoSelection("마법사")}
+          text="마법사"
+          bgColor="bg-indigo-700"
+        />
+        <Button
+          onPress={() => handleWhoSelection("아이돌")}
+          text="아이돌"
+          bgColor="bg-pink-700"
+        />
+        <Button
+          onPress={() => handleWhoSelection("동화 속 캐릭터")}
+          text="동화 속 캐릭터"
+          bgColor="bg-teal-700"
         />
       </div>
     </div>

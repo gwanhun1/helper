e08700f1e6ac9ff8a1048bde1eeb0cc41fddb042
gwanhun1 <wjdrhksgns7602@gmail.com>
2024-12-05 14:02:49 +0900

    feature: rn-helper 리액트 마이그레이션 작업 12/5

--- src/components/atoms/Loading.tsx
+++ src/components/atoms/Loading.tsx
@@ -1,30 +1,12 @@
-import { useEffect, useState } from "react";
 import Lottie from "lottie-react";
 import tree from "./tree.json";
 
 const Loading = () => {
-  const [viewportHeight, setViewportHeight] = useState("60vh");
-
-  useEffect(() => {
-    const updateViewportHeight = () => {
-      setViewportHeight(`${window.innerHeight}px`);
-    };
-
-    updateViewportHeight();
-    window.addEventListener("resize", updateViewportHeight);
-
-    return () => {
-      window.removeEventListener("resize", updateViewportHeight);
-    };
-  }, []);
-
   return (
-    <div style={{ height: viewportHeight }}>
       <Lottie
         animationData={tree}
         style={{ height: "220px", margin: "60px" }}
       />
-    </div>
   );
 };
 
--- src/components/organisms/ForestLog.tsx
+++ src/components/organisms/ForestLog.tsx
@@ -2,18 +2,18 @@ import useLogData from "../../hooks/useLogData";
 import LogForm from "./LogForm";
 
 const ForestLog = () => {
-  const { data: forestData } = useLogData();
+  const { data: forestData, refreshData } = useLogData();
 
   return (
     <div
-      className="h-1/5 max-h-[600px] overflow-y-auto"
+      className="h-1/5 max-h-[600px] overflow-y-auto "
       style={{
         height: "500px",
         minHeight: "500px",
       }}
     >
       {forestData.map((e) => (
-        <LogForm key={e.id} data={e} />
+        <LogForm key={e.id} data={e} onDelete={refreshData} />
       ))}
     </div>
   );

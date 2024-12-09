c903294f448327ede50c86bedee687fc0dcbf87c
gwanhun1 <wjdrhksgns7602@gmail.com>
2024-12-09 10:30:57 +0900

    feature: rn-helper 리액트 마이그레이션 작업 12/9

---
+++ src/components/molecules/AuthAlert.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import useUserStore from "../../store/userStore";
+import Snackbar from "../atoms/Snackbar";
+
+const AuthAlert = () => {
+  const [showSnackbar, setShowSnackbar] = useState(false);
+  const user = useUserStore((state) => state.user);
+
+  useEffect(() => {
+    if (!user) {
+      setShowSnackbar(true);
+    }
+  }, [user]);
+
+  return (
+    <Snackbar 
+      show={showSnackbar}
+      message="로그인하고 더 많은 기능을 이용해보세요 ✨"
+      onClose={() => setShowSnackbar(false)}
+    />
+  );
+};
+
+export default AuthAlert;
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -2,6 +2,7 @@ import PageLayout from "../organisms/PageLayout";
 import Forest from "../organisms/Forest";
 import ForestLog from "../organisms/ForestLog";
 import MindTemplate from "../organisms/MindTemplate";
+import AuthAlert from "../molecules/AuthAlert";
 
 const Home = () => {
   return (
@@ -9,10 +10,9 @@ const Home = () => {
       <div className="flex flex-col">
         <MindTemplate/>
         <Forest />
-        
-          <ForestLog />
-        
+        <ForestLog />
       </div>
+      <AuthAlert />
     </PageLayout>
   );
 };

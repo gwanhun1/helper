1d52379f5d1725d014f1a5d7e1851aed583ad919
정관훈 <wjdrhksgns7602@gmail.com>
2024-12-11 17:21:56 +0900

    feature: rn-helper 리액트 마이그레이션 작업 12/11

--- src/components/pages/Auth.tsx
+++ src/components/pages/Auth.tsx
@@ -4,7 +4,9 @@ const Auth = () => {
   return (
     <div className="h-full bg-green">
       <div className="flex items-center justify-center h-5/6">
-        <div className="text-4xl font-bold text-white">HELPER</div>
+        <div className="text-4xl font-bold text-white helper-text">
+          MoodMentor
+        </div>
       </div>
       <div className="flex items-center justify-center h-1/6">
         <KakaoLogin />

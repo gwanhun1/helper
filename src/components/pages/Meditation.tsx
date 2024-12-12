5fc28b2e38810701b84e28fe13fd2fe51fd4dc37
gwanhun1 <wjdrhksgns7602@gmail.com>
2024-12-12 13:37:53 +0900

    feature: rn-helper 리액트 마이그레이션 작업 12/12

--- src/components/pages/Meditation.tsx
+++ src/components/pages/Meditation.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import MeditationSession from '../organisms/MeditationSession';
+import React from "react";
+import MeditationSession from "../organisms/MeditationSession";
 
 const MeditationPage: React.FC = () => {
   return (
